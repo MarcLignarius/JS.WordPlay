9251393150f570a614a079170e03d84360f66b30
Marc Davies <marcdaviesriot@gmail.com>
2020-01-13 13:55:45 -0800

    add regex expression to strip user input from any punctuation and symbols

--- js/scripts.js
+++ js/scripts.js
@@ -1,15 +1,15 @@
 $(function() {
     $("#userForm").submit(function(event) {
         event.preventDefault();
-        let userInput = $("#userInput").val();
+        let userInput = $("#userInput").val().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '');
         console.log('userInput', userInput);
         let inputToArray = userInput.split(' ');
-        console.log('inputToArray', inputToArray)
+        console.log('inputToArray', inputToArray);
         let filteredArray = inputToArray.filter(function(word) {
             if (word.length > 3) {
                 return word;
-            } 
-        })
-        console.log('filteredArray', filteredArray)
+            }
+        });
+        console.log('filteredArray', filteredArray);
     })
 });
