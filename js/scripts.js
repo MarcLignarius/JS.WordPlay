3cd7d89f067035f275e6eaaf5f562e64e8635c58
Marc Davies <marcdaviesriot@gmail.com>
2020-01-13 13:46:12 -0800

    filter array to only include words of 3 characters or more and log them to the console

--- js/scripts.js
+++ js/scripts.js
@@ -3,7 +3,13 @@ $(function() {
         event.preventDefault();
         let userInput = $("#userInput").val();
         console.log('userInput', userInput);
-        let userInputArray = userInput.split(' ');
-        console.log('userInputArray', userInputArray)
+        let inputToArray = userInput.split(' ');
+        console.log('inputToArray', inputToArray)
+        let filteredArray = inputToArray.filter(function(word) {
+            if (word.length > 3) {
+                return word;
+            } 
+        })
+        console.log('filteredArray', filteredArray)
     })
 });
