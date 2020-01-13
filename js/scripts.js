9bf8f5e8aba26887157ef55b541a54362c63fdaf
Marc Davies <marcdaviesriot@gmail.com>
2020-01-13 14:09:06 -0800

    add comments to script

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,19 @@
 $(function() {
-    $("#userForm").submit(function(event) {
-        event.preventDefault();
-        let userInput = $("#userInput").val().replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, '');
-        console.log('userInput', userInput);
-        let inputToArray = userInput.split(' ');
-        console.log('inputToArray', inputToArray);
-        let filteredArray = inputToArray.filter(function(word) {
-            if (word.length > 3) {
-                return word;
-            }
-        });
-        console.log('filteredArray', filteredArray);
-        let finalArray = filteredArray.reverse().join(' ');
-        console.log('finalArray', finalArray);
-    })
+  $("#userForm").submit(function(event) {
+    event.preventDefault();
+    //retrieve sentence entered by user, save it as userInput and remove punctuation
+    let userInput = $("#userInput")
+      .val()
+      .replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()@\+\?><\[\]\+]/g, "");
+    //split userInput into an array of strings, using white space as the separator and save it as inputToArray
+    let inputToArray = userInput.split(" ");
+    //filter the array for words that are 3 or more characters in length and save it as filteredArray
+    let filteredArray = inputToArray.filter(function(word) {
+      if (word.length >= 3) {
+        return word;
+      }
+    });
+    //reverse the filtered array, turn it back into a string and save it as finalArray
+    let finalArray = filteredArray.reverse().join(" ");
+  });
 });
