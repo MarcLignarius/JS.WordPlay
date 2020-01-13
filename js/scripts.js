d4c26e73e558c647f806ba3210d438907b135394
Marc Davies <marcdaviesriot@gmail.com>
2020-01-13 13:28:36 -0800

    get user submission and log it to the console

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,7 @@
 $(function() {
-
+    $("#userForm").submit(function(event) {
+        event.preventDefault();
+        let userInput = $("#userInput").val();
+        console.log(userInput);
+    })
 });
